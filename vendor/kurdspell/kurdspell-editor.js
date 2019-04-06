b13357047160ede20366f16d13fab77e5ed1158f
aram rafeq <aramrafeq2@gmail.com>
2019-04-06 09:50:51 +0300

    editor improved

--- vendor/kurdspell/kurdspell-editor.js
+++ vendor/kurdspell/kurdspell-editor.js
@@ -1,60 +1,75 @@
 
 function kurdspellEditor(selector){
-    $(selector).on("keyup", function(e){
-        if (e.keyCode == 32){
-            var text = $(this).text().replace(/[\s]+/g, " ");
-            var word = text.split(" ");
-            var newHTML = "";
-            var el = this
-            if(text.trim()!==""){
+    
+    var spellCheck = function (text, el){
+        var word = text.split(" ");
+        var newHTML = "";
+        $.ajax({
+            type: "GET",
+            url: "https://kurdspell.azurewebsites.net/api/spell/"+text,
+            success: function(response){
+                $.each(word, function(index, value){   
+                    newHTML += "<div class='other'>" + value + "&nbsp;</div>";
+                });            
+                $.each(response, function(errorIndex, error){
+                    current = "<div class='other'>" + error.original + "&nbsp;</div>"
+                    var re = new RegExp(current, 'g');
+                    suggestionsHTML = ""
+                    $.each(error.suggestions, function(suggestionIndex, suggestion){
+                        suggestionsHTML += "<a href='#' data-error='error-"+error.original+"' class='suggestion-element' >"+suggestion +"</a><br>";
+                    })
+                    newHTML = newHTML.replace(re , `
+                        <span data-suggestions='${error.suggestions.join(';')}' class='error error-${error.original} '
+                            data-toggle="popover" 
+                            title="پێشنیارەکان" 
+                            data-placement="bottom"                                        
+                            data-content="
+                                ${suggestionsHTML}
+                            "
+                        > 
+                        ${error.original}
+                        </span> 
+                    ` );
+                }) 
+                $(el).html(newHTML);
+                
+                var child = $(el).children();
+                var range = document.createRange();
+                var sel = window.getSelection();
+                range.setStart(child[child.length - 1], 1);
+                range.collapse(true);
+                sel.removeAllRanges();
+                sel.addRange(range);
+                $(el)[0].focus(); 
+                $('[data-toggle="popover"]').popover({
+                    html:true
+                })
 
-                $.ajax({
-                        type: "GET",
-                        url: "https://kurdspell.azurewebsites.net/api/spell/"+text,
-                        success: function(response){
-                            $.each(word, function(index, value){   
-                                newHTML += "<div class='other'>" + value + "&nbsp;</div>";
-                            });            
-                            $.each(response, function(errorIndex, error){
-                                current = "<div class='other'>" + error.original + "&nbsp;</div>"
-                                var re = new RegExp(current, 'g');
-                                suggestionsHTML = ""
-                                $.each(error.suggestions, function(suggestionIndex, suggestion){
-                                    suggestionsHTML += "<li>"+suggestion +"</li>";
-                                })
-                                newHTML = newHTML.replace(re , `
-                                    <span data-suggestions='${error.suggestions.join(';')}' class='error'> 
-                                        <p style="width: max-content;">${error.original}</p>
-                                        <ul class="suggestion-box">
-                                            ${suggestionsHTML}
-                                        </ul>
-                                    </span> 
-                                ` );
-                            }) 
-                            $(el).html(newHTML);
-                            
-                            var child = $(el).children();
-                            var range = document.createRange();
-                            var sel = window.getSelection();
-                            range.setStart(child[child.length - 1], 1);
-                            range.collapse(true);
-                            sel.removeAllRanges();
-                            sel.addRange(range);
-                            $(el)[0].focus(); 
-                        }
-                });           
             }
-            
-        }
+        });
+    }
+    var typingTimer;                //timer identifier
+    var doneTypingInterval = 1000;  //time in ms, 5 second for example
+    var $input = $(selector);
+    $input.on('keyup', function () {
+        clearTimeout(typingTimer);
+        var s = this
+        typingTimer = setTimeout(()=>doneTyping(s), doneTypingInterval);
     });
-    $(selector).on('click','.error .suggestion-box li',function(){
+    $input.on('keydown', function () {
+        clearTimeout(typingTimer);
+    });
+    function doneTyping (selector) {
+        var text = $(selector).text().replace(/[\s]+/g, " ");
+        spellCheck(text,selector)
+    }
+    $(document).on('click','.suggestion-element',function(){
         var text = $(this).text()
-        var err = $(this).parents('.error')
-        var p = $(err).find('p')
-        console.log(err)
-        $(p).html(text)
-        $(this).parents('.suggestion-box').remove()
-        $(err).addClass('other').removeClass('error')
+        var err = $(this).data('error')
+        $("."+err).html(text)
+        $('.popover').remove()
+        $("."+err).addClass('other').removeClass('error')
     })
         
 }
+
